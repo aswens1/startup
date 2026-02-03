280add0fc8cdb1074b854dc861acdbe8bf2e48c8
alyseswenson <alyse.swenson@gmail.com>
2026-02-02 19:51:46 -0700

    hopefully fixed tailwind issue

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        collabCanvas: './pages/collaborativeCanvas.html',
+        gamePlayCanvas: './pages/gamePlayCanvas.html',
+        gamePlaySelection: './pages/gameSelectionMenu.html',
+        getStarted: './pages/getStarted.html',
+        leaderboard: './pages/leaderboard.html',
+        // List all other files here
+      },
+    }
+  }
 })
