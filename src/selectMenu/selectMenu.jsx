5c5f4f2522a256dc7de3bbc115291006c741315e
alyseswenson <alyse.swenson@gmail.com>
2026-02-07 11:26:35 -0700

    starter component made for each page

---
+++ src/selectMenu/selectMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function gameSelectionMenu() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>game menu displayed here</div>
+    </main>
+  );
+}
