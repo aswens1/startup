319b722ef0d106a07bdbb6d48c8bab06eee6090c
alyseswenson <alyse.swenson@gmail.com>
2026-02-08 16:57:14 -0700

    leaderbord title

--- src/leaderboard/leaderboard.jsx
+++ src/leaderboard/leaderboard.jsx
@@ -11,10 +11,10 @@ export function Leaderboard() {
       <main className="min-h-[calc(100vh-160px)] flex justify-center pt-20 pb-20">
         <div className="bg-white/85 rounded-xl px-8 py-16 max-w-3xl w-full flex justify-center flex-col items-center text-center md:gap-y-5">
 
-            <h1 className="font-['Jersey_10'] text-[7rem] tracking-wider leading-[0.9]">Leaderboard</h1>
+            <h1 className="font-['Jersey_10'] text-[rem] tracking-wider leading-[0.9]" id="main-title">Leaderboard</h1>
             <p className="mt-6 text-xl">Place more pixels in the lobby and the games to see your name on the leaderboard!</p>
 
-            <div className="leaderboard-scroll">
+            <div className="leaderboard-scroll mt-0">
                 <table className="w-full border-separate border-spacing-x-6">
                     <thead className="text-xl">
                         <tr>
