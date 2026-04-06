5079f4cc3c5dc2c56e80df5172dc2e371aca48a3
alyseswenson <alyse.swenson@gmail.com>
2026-04-06 16:01:23 -0600

    added ws file

---
+++ src/websocket.jsx
@@ -0,0 +1,16 @@
+export function getSocket() {
+    if (!socket) {
+        const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+        socket = new WebSocket(`${protocol}://${window.location.host}`);
+        
+        socket.onopen = () => {
+            console.log('WebSocket connected');
+        };
+
+        socket.onerror = (err) => {
+            console.error('WebSocket error: ', err)
+        }
+    }
+}
+
+return socket;
